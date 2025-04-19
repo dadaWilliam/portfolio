07ccacb64b10bf532be04511dc683a563fbcaacd
dadaWilliam <90446055+dadaWilliam@users.noreply.github.com>
2025-04-19 02:31:01 -0400

    add sitemap, robots.txt and favicon.ico

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://will.xueba.ca',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
